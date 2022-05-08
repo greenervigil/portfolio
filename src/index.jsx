702eff59e86b749f68cd527bd7b5086efbfcc9d4
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-05-07 21:38:00 -0400

    removing package dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Exp from './components/exp';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import About from './components/about';
-import Home from './components/home';
-import Layout from './components/layout';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 
@@ -15,17 +9,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/experience" element={<Exp />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Route>
-      </Routes>
-        </BrowserRouter>
+      <App/>
   </React.StrictMode>
 );
 
