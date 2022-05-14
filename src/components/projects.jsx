a6a564e33752b42b45e406338709497b6f02eba8
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-05-14 00:14:32 -0400

    adding in projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
-import { CSSRulePlugin } from 'gsap-trial/CSSRulePlugin';
 import styled from "@emotion/styled";
 
 import animation from '../assets/images/animation_calculator.png';
@@ -9,140 +6,166 @@ import bigIdeas from '../assets/images/big_ideas_campaign.png';
 import digitalAccess from '../assets/images/digital_access_services_campaign.png';
 import expressAnimations from '../assets/images/express_animations.png';
 
-gsap.registerPlugin(CSSRulePlugin);
 
-const Container = styled.div`
-    padding-top: 100px;
-    width:100%;
-    height: 100%;
-    transform-style: preserve-3d;
-    user-select:none;
+const Container = styled.ul`
+    overflow:hidden;
+    width:90%;
+    margin:0 auto; 
 
-
-    div, svg {
-        position: absolute;
+    .clear:after {
+        content:"";
+        display:block;
+        clear:both;
     }
 
-    .container {
-        perspective: 2000px;
-        width: 300px;
-        height: 400px;  
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,-50%);
-
-        .ring, img {
-            width:100%;
-            height: 100%;
-            transform-style: preserve-3d;
-            user-select:none;
-        }
+    li {
+        position:relative;
+        list-style-type:none;
+        width:27.85714285714286%; /* = (100-2.5) / 3.5 */
+        padding-bottom: 32.16760145166612%; /* =  width /0.866 */
+        float:left;
+        overflow:hidden;
+        visibility:hidden;
+        
+        -webkit-transform: rotate(-60deg) skewY(30deg);
+        -ms-transform: rotate(-60deg) skewY(30deg);
+        transform: rotate(-60deg) skewY(30deg);
     }
 
+    li:nth-child(3n+2){
+        margin:0 1%;
+    }
+
+    li:nth-child(6n+4){
+        margin-left:0.5%;
+    }
+
+    li:nth-child(6n+4), li:nth-child(6n+5), li:nth-child(6n+6) {
+        margin-top: -6.9285714285%;
+        margin-bottom: -6.9285714285%;
+        
+        -webkit-transform: translateX(50%) rotate(-60deg) skewY(30deg);
+        -ms-transform: translateX(50%) rotate(-60deg) skewY(30deg);
+        transform: translateX(50%) rotate(-60deg) skewY(30deg);
+    }
+
+    li:nth-child(6n+4):last-child, li:nth-child(6n+5):last-child, li:nth-child(6n+6):last-child {
+        margin-bottom:0%;
+    }
+
+    li * {
+        position:absolute;
+        visibility:visible;
+    }
+
+    li > div {
+        width:100%;
+        height:100%;
+        text-align:center;
+        color:#fff;
+        overflow:hidden;
+        
+        -webkit-transform: skewY(-30deg) rotate(60deg);
+        -ms-transform: skewY(-30deg) rotate(60deg);
+        transform: skewY(-30deg) rotate(60deg);
+        
+        -webkit-backface-visibility:hidden;
+        
+    }
+
+    li img {
+        left:-100%; right:-100%;
+        width: auto; height:100%;
+        margin:0 auto;   
+    }
+
+    div h1, div p {
+        width:90%;
+        padding:0 5%;
+        background-color:#18cae6;
+        font-family: 'Raleway', sans-serif;
+        
+        -webkit-transition: top .2s ease-out, bottom .2s ease-out, .2s padding .2s ease-out;
+        -ms-transition: top .2s ease-out, bottom .2s ease-out, .2s padding .2s ease-out;
+        transition: top .2s ease-out, bottom .2s ease-out, .2s padding .2s ease-out;
+    }
+
+    li h1 {
+        bottom:110%;
+        font-style:italic;
+        font-weight:normal;
+        font-size:1.5em;
+        padding-top:100%;
+        padding-bottom:100%;
+    }
+
+    li h1:after {
+        content:'';
+        display:block;
+        position:absolute;
+        bottom:-1px; left:45%;
+        width:10%;
+        text-align:center;
+        z-index:1;
+        border-bottom:2px solid #fff;
+    }
+
+    li p {
+        padding-top:50%;
+        top:110%;
+        padding-bottom:50%;
+    }
+
+    li div:hover h1 {
+        bottom:50%;
+        padding-bottom:10%;
+    }
+
+    li div:hover p{
+        top:50%;
+        padding-top:10%;
+    }
 `;
 
 const Projects = () => {
-    let xPos = 0;
-    const tl = useRef();
-
-    function getBgPos(i){
-        return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
-    }
-
-    function drag(e){
-        if (e.touches) e.clientX = e.touches[0].clientX;    
-      
-        gsap.to('.ring', {
-          rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
-          onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBgPos(i) }) }
-        });
-        
-        xPos = Math.round(e.clientX);
-    }
-
-    useEffect(() => {
-        tl.current = gsap.timeline().set('.ring', {
-            rotationY: 450,
-            cursor: 'grab',
-        })
-        .set('.img', {
-            rotateY: (i) => i*-36,
-            transformOrigin: '50% 50% 500px',
-            z: -500,
-            backgroundPosition:(i)=>getBgPos(i),
-            backfaceVisibility: 'hidden'
-        })
-        .from('.img', {
-            duration: 1.5,
-            y: 200,
-            opacity: 0,
-            stagger: 0.1,
-            ease: 'expo'
-        })
-        .add(() => {
-            const enter = document.querySelector('.img');
-
-            enter.addEventListener('mouseenter', (e) => {
-                let current = e.currentTarget;
-                console.log('event')
-                gsap.to('.img', {
-                    opacity:    (i,t) => (t===current) ? 1:0.5, 
-                    ease: 'power3'
-                })
-            })
-
-            enter.addEventListener('mouseleave', (e) => {
-                gsap.to('.img', {opacity:1, ease:'power2.inOut'})
-            })
-        }, '-=0.5')
-    }, []);
     return (
-        <Container id="projects">
-            
-            <div className="container">
-                <div className="ring" ref={tl}>
-                    <div className="img">
-                        <img 
-                            src={animation} 
-                            alt={'Creative Studios animation Calculator tool for their wordpress site'}
-                            width={400}
-                            height={600}
-                        />
-                    </div>
-                    <div className="img">
-                        <img 
-                            src={nglMarketing} 
-                            alt={'National geographic Marketing Site for Secondary Education'}
-                            width={400}
-                            height={600}
-                        />
-                    </div>
-                    <div className="img">
-                        <img 
-                            src={bigIdeas} 
-                            alt={'National geographic Marketing Site Big Ideas Mathematics Campaign'}
-                            width={400}
-                            height={600}
-                        />
-                    </div>
-                    <div className="img">
-                        <img 
-                            src={digitalAccess} 
-                            alt={'National geographic Marketing Site Digital Access Campaign'}
-                            width={400}
-                            height={600}
-                        />
-                    </div>
-                    <div className="img">
-                        <img 
-                            src={expressAnimations} 
-                            alt={'Creative Studios Wordpress Pages for Express Animations'}
-                            width={400}
-                            height={600}
-                        />
-                    </div>
+        <Container className='clear'>
+            <li className='pusher'></li>
+            <li>
+                <div>
+                    <img src={animation} alt="Creative Studios animation calculator tool wordpress page" />
+                    <h1>Creative Studios - Cengage Group</h1>
+                    <p>Animation Calculator Tool Wordpress page</p>
                 </div>
-            </div>
+            </li>
+            <li>
+                <div>
+                    <img src={nglMarketing} alt="National Geographic Learning Marketing site for Secondary Education" />
+                    <h1>NGL Marketing - Cengage Group</h1>
+                    <p>National Geographic Learning Marketing site for Secondary education.</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img src={bigIdeas} alt="Big Ideas Mathematics Marketing campaign for National Geographic Learning" />
+                    <h1>NGL Big Ideas Mathematics - Cengage Group</h1>
+                    <p>Big Ideas 7 page Marketing Campaign</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img src={digitalAccess} alt="National Geographic Learning Digital Access Campaign for secondary education" />
+                    <h1>NGL Digital Access - Cengage Group</h1>
+                    <p>6 Page Digital Access Campaign for National Geographic Learning Secondary Education</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img src={expressAnimations} alt="Creative Studios Wordpress site" />
+                    <h1>Creative Studios - Cengage Group</h1>
+                    <p>Additional pages to Creative Studios wordpress site.</p>
+                </div>
+            </li>
         </Container>
     );
 }
