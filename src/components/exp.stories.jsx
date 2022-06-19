564d11161ce0bb03436b3026f7b3b2b5098e2a97
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-06-18 22:30:42 -0400

    create exp story

---
+++ src/components/exp.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Exp from './exp';
+
+export default {
+  title: 'Section/Experience',
+  component: Exp,
+};
+
+const Template = (args) => <Exp {...args} />;
+
+export const Primary = Template.bind({});
