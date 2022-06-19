e98e44ddbe717a58bc67e6a0d5bf7c9e7e2d84f9
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-06-19 01:17:05 -0400

    refactoring the card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,10 +3,7 @@ import styled from '@emotion/styled';
 const Div = styled.div`
     min-height: 550px;
     width: 50vw;
-    /*background-color: #000;*/
     color: #fff;
-    /*border: 1px solid #18cae6;*/
-    /*box-shadow: 10px 5px 5px #18cae6;*/
     margin: 0 auto;
     text-align: center;
     text-shadow: 0 0 10px;
