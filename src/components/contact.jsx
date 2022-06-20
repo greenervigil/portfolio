25a061cc2a05c0e78e935fbc3e1636e53e1cc8f6
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-06-19 23:05:03 -0400

    submit button error fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -78,6 +78,25 @@ const StyledDiv = styled.div`
            textarea {
             width: 75%;
            }
+
+          button {
+            background-color: #18cae6;
+            border: 1px solid #18cae6;
+            border-radius: 10px;
+            color: #000;
+            height: 31px;
+            padding: 0 10px;
+            text-shadow: 0 0 10px;
+          }
+
+          button:hover {
+            background-color: rgba( 104, 215, 217, 0.15 );
+            border: 1px solid #18cae6;
+            border-radius: 8px;
+            color: #18cae6;
+            text-shadow: 0 0 10px;
+            box-shadow: 0 8px 32px 0 #18cae6;
+          }
         }
       }
       
@@ -199,7 +218,7 @@ const Contact = () => {
                   <input type="text" name="_honey" style={{ display: 'none' }} />
                   <input type="hidden" name="_template" value="box"></input>
                   
-                  <Button type="submit" label="Submit" />
+                  <button type="submit">Submit</button>
                   <span className='close' onClick={closeCard}>X</span>
                 </form>
               </div>
