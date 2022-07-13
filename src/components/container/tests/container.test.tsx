8e244dab94f40f022f787b33b8b281613162a702
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-07-12 23:52:35 -0400

    adding unit tests

--- src/components/container/tests/container.test.tsx
+++ src/components/container/tests/container.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { getByTestId, render, screen } from "@testing-library/react";
 import Container from "../container";
 
 describe('*** Container Component ***', () => {
     it('renders the component', () => {
         render(<Container />)
 
-        expect(screen.getByTestId('container')).toBeInTheDocument();
+        expect(screen.getByTestId('container')).toBeInTheDocument()
+    })
+    it('renders children', () => {
+        render(<Container children={<div><h1 data-testid="header">Test</h1></div>}/>)
+
+        const header = screen.getByTestId('header')
+
+        expect(screen.getByTestId('container')).toContainElement(header)
     })
 })
