ff9d4494c1bd5dba47035fd4c55d55fb9c5f697e
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-09-01 23:56:24 -0400

    clean up

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,4 @@
 import Button from "./button/button";
-import Card from "./card/card";
 
 import animation from '../assets/images/animation_calculator.png';
 import nglMarketing from '../assets/images/ngl_marketing.png';
@@ -8,7 +7,7 @@ import digitalAccess from '../assets/images/digital_access_services_campaign.png
 import expressAnimations from '../assets/images/express_animations.png';
 import galsBestPals from '../assets/images/gals_best_pal.png'
 
-const ImageCard = ({ detail, title, background }) => {
+const ImageCard = ({ detail, title, background, location }) => {
     return (
         <div style={{ width: '280px', height: '400px', color: '#fff', margin: '0 auto',
         textAlign: 'center',
@@ -20,10 +19,12 @@ const ImageCard = ({ detail, title, background }) => {
         backdropFilter: 'blur( 5px )',
         borderRadius: '10px',
         border: '1px solid rgba( 255, 255, 255, 0.18 )' }}>
-            <img src={background} width={200} height={200}/>
+            <img src={background} width={200} height={200} style={{ borderRadius: '12px' }} alt="" />
             <h3>{title}</h3>
             <p>{detail}</p>
-            <Button label="Read More" />
+            <a target="_blank" rel="noreferrer" href={location}>
+                <Button label="Go to Site" />
+            </a>
         </div>
     )
 }
@@ -32,15 +33,15 @@ const Projects = () => {
     return (
         <>
             <div style={{ display: 'flex', justifyContent: 'space-around', margin: '100px'}}>
-                <ImageCard title="Animation Calculator" background={animation} detail={'Creative Studios Animations Calculator - Wordpress Page tool to calculate animation requests'}/>
-                <ImageCard title="National Geographic Learning K-12" background={nglMarketing} detail={' Marketing site'}/>
-                <ImageCard title="NGL K-12 Big Ideas Marketing Campaign pages" background={bigIdeas} detail={'Big Ideas 9 page marketing campaign'}/>
+                <ImageCard title="Animation Calculator" background={animation} detail={'Creative Studios Animations Calculator - Wordpress Page tool to calculate animation requests'} location={'https://cs.cengage.com/animation-calculator'}/>
+                <ImageCard title="National Geographic Learning K-12" background={nglMarketing} detail={' Marketing site'} location={'https://ngl.cengage.com'}/>
+                <ImageCard title="NGL K-12 Big Ideas Marketing Campaign pages" background={bigIdeas} detail={'Big Ideas 9 page marketing campaign'} location={'https://ngl.cengage.com/assets/html/bigIdeas'}/>
                 
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-around', margin: 'auto 150px'}}>
-                <ImageCard title="NGL Digital Access Campaign" detail={'Digital Access 6 Page Promotion'} background={digitalAccess} />
-                <ImageCard title="Creative Studios Wordpress" detail={'Updates to the Creative Studios wordpress site'} background={expressAnimations} />
-                <ImageCard title="Gals Best Pal's Company Site" detail={'Wordpress Theme for Gals Best Pal Dog Fostering Team'} background={galsBestPals} />
+                <ImageCard title="NGL Digital Access Campaign" detail={'Digital Access 6 Page Promotion'} background={digitalAccess} location={'https://ngl.cengage.com/assets/html/digitalAccess'}/>
+                <ImageCard title="Creative Studios Wordpress" detail={'Updates to the Creative Studios wordpress site'} background={expressAnimations} location={'https://cs.cengage.com'}/>
+                <ImageCard title="Gals Best Pal's Company Site" detail={'Wordpress Theme for Gals Best Pal Dog Fostering Team'} background={galsBestPals} location={'https://galsbestpal.com'}/>
             </div>
         </>
     )
